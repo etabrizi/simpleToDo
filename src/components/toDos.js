2117ac7f50b87fea4404a8bcf3be371c209b3c89
Emyr Tabrizi <etabrizi1@gmail.com>
2018-09-14 15:06:11 +0100

    clean ups

--- src/components/toDos.js
+++ src/components/toDos.js
@@ -2,12 +2,10 @@ import React from 'react'
 
 const ToDos = ({ todos, removeToDo, disableToDo }) => {
 
-    console.log(todos);
-
     const todoList = todos.length ? (
         todos.map(todos => {
             return (<p key={todos.id}><span className={todos.active ? 'active spaced-right' : 'disabled spaced-right'}>{todos.content}</span>
-                <button className="btn-small btn-floating spaced-right-sm" onClick={(event) => { disableToDo(event, todos.id) }}><i class="material-icons">check</i></button>
+                <button className="btn-small btn-floating spaced-right-sm" onClick={(event) => { disableToDo(event, todos.id) }}><i className="material-icons">check</i></button>
                 <button className="btn-small btn-floating" onClick={(event) => { removeToDo(event, todos.id) }}>x</button></p>
             )
         })
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import { Provider } from 'react-redux'
 import rootReducer from './reducers/reducer'
+import thunk from 'redux-thunk'
 
-const store = createStore(rootReducer);
+
+const middleware = applyMiddleware(thunk)
+const store = createStore(rootReducer, middleware);
 
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
